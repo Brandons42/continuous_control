30f090dfcce6eeb643a30ea7b7457475cbc5dfda
Brandon Suen <brandonsuendeveloper@gmail.com>
2020-06-29 19:27:36 -0700

    Code refactored; fixed noise generation bug

--- noise.py
+++ noise.py
@@ -18,7 +18,7 @@ class OUNoise:
 		self.mu = mu * np.ones(size)
 		self.theta = theta
 		self.sigma = sigma
-		self.seed = random.seed(seed)
+		random.seed(seed)
 		self.reset()
 
 	def reset(self):
@@ -28,6 +28,6 @@ class OUNoise:
 	def sample(self):
 		"""Update internal state and return it as a noise sample."""
 		x = self.state
-		dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+		dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
 		self.state = x + dx
 		return self.state
